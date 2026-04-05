9748e386a85657328c0e5b708186d05d1afc1dc2
Cyborg99 <ahmad.husien4@gmail.com>
2026-04-05 19:41:28 +0300

    fix: implement inventory update logic

--- PerfumeStore.App/PerfumeStore.Application/Inventory/Commands/UpdateInventory/UpdateInventoryCommand.cs
+++ PerfumeStore.App/PerfumeStore.Application/Inventory/Commands/UpdateInventory/UpdateInventoryCommand.cs
@@ -2,7 +2,9 @@
 using PerfumeStore.Application.Inventory.Dtos;
 
 namespace PerfumeStore.Application.Inventory.Commands.UpdateInventory {
-    public class UpdateInventoryCommand : IRequest<InventoryDto> {
+    public class UpdateInventoryCommand : IRequest {
         public int ProductID { get; set; }
+        public int Quantity { get; set; }
+
     }
 }
--- PerfumeStore.App/PerfumeStore.Application/Inventory/Commands/UpdateInventory/UpdateInventoryCommandHandler.cs
+++ PerfumeStore.App/PerfumeStore.Application/Inventory/Commands/UpdateInventory/UpdateInventoryCommandHandler.cs
@@ -1,16 +1,16 @@
 ﻿using AutoMapper;
 using MediatR;
-using PerfumeStore.Application.Inventory.Dtos;
 using PerfumeStore.Domain.Exceptions;
 using PerfumeStore.Domain.Repositories;
 
 namespace PerfumeStore.Application.Inventory.Commands.UpdateInventory {
-    public class UpdateInventoryCommandHandler(IInventoryRepository inventoryRepository, IMapper mapper) : IRequestHandler<UpdateInventoryCommand, InventoryDto> {
-        public async Task<InventoryDto> Handle(UpdateInventoryCommand request, CancellationToken cancellationToken) {
+    public class UpdateInventoryCommandHandler(IInventoryRepository inventoryRepository, IMapper mapper) : IRequestHandler<UpdateInventoryCommand> {
+        public async Task Handle(UpdateInventoryCommand request, CancellationToken cancellationToken) {
             var inventory = await inventoryRepository.GetByIdAsync(request.ProductID);
             if (inventory == null) 
                 throw new NotFoundException(nameof(Domain.Entities.Inventory), request.ProductID.ToString());
-            return mapper.Map<InventoryDto>(inventory);
+            mapper.Map(request, inventory);
+            await inventoryRepository.SaveChangesAsync();
         }
     }
 }
